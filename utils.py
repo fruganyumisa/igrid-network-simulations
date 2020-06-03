e2412655bc2b50f91f4b08b380069604216417ab
bcmmbaga <b@infolabs.dev>
2020-06-03 18:35:12 +0300

    add devices to mininet.net object

--- utils.py
+++ utils.py
@@ -1,31 +1,45 @@
-def spawnStations(stations=0, smart_meters=0, actuator=0, mac_prefix="00:00:00:00:00"):
+def spawnStations(net, stations=0, smart_meters=0, actuator=0, mac_prefix="00:00:00:00:00", ip_prefix="10.0.0", cidr="/1"):
     """
+    spawnStations generate specified number of IoT devices (stations) and assign to them (stations) 
+    specific mac address and ip address based on mac_prefic and ip_prefix
 
+    mac_prefix defines first 5 octet number(s) in mac address. The last octect will be generated from device index.
+
+    ip_prefix  defines the network address. The host address will be generated from device index.
     """
     # ip = "10.0.0.1/8"
     # position = "10,20,0"
-    # antenna_height = "1"
-    # antenna_gain = "5"
-    # active_scan = 1
-    # scan_freq = "2412 2437 2462"
-     
-    #  generate smart meters 
-    generate(device_control=0, device_total=smart_meters, prefix_name="smeter", mac_prefix=mac_prefix)
+
+    #  generate smart meters
+    net = generate(net, device_control=0, device_total=smart_meters,
+                   prefix_name="smeter", mac_prefix=mac_prefix, ip_prefix=ip_prefix, cidr=cidr)
 
     # generate actuator
-    generate(device_control=smart_meters, device_total=actuator, prefix_name="actuator", mac_prefix=mac_prefix)
+    net = generate(net, device_control=smart_meters, device_total=actuator,
+                   prefix_name="actuator", mac_prefix=mac_prefix, ip_prefix=ip_prefix, cidr=cidr)
 
     # generate stations
-    generate(device_control=smart_meters + actuator, device_total=stations, prefix_name="sta", mac_prefix=mac_prefix)
+    net = generate(net, device_control=smart_meters + actuator,
+                   device_total=stations, prefix_name="sta", mac_prefix=mac_prefix, ip_prefix=ip_prefix, cidr=cidr)
 
 
-def generate(device_control, device_total, prefix_name, mac_prefix):
+def generate(net, device_control, device_total, prefix_name, mac_prefix, ip_prefix, cidr):
+    """
+    generate creates station and add it to mininet.net object which contain all spawned network devices.
+    """
     for i in range(device_total):
         device_control += 1
         device_name = f'{prefix_name}{i + 1}'
         mac_addr = f'{mac_prefix}:{"{:0>2d}".format(device_control)}'
+        ip_addr = f'{ip_prefix}.{device_control}{cidr}'
+        pos = f'{device_control},{device_control},0'
 
-        print("Device: ", device_name, "mac_addr:", mac_addr)
+        print("Device: ", device_name, "mac_addr:",
+              mac_addr, "ip_addr:", ip_addr, "position:", pos)
+        net.addStation(device_name, mac=mac_addr, ip=ip_addr,
+                       antennaHeight='1', antennaGain='5',  position=pos, active_scan=1, scan_freq="2412 2437 2462")
+    return net
+
 
 if __name__ == "__main__":
-    spawnStations(stations=10, smart_meters=3, actuator=5)
+    spawnStations("Hello", stations=10, smart_meters=2, actuator=5)
