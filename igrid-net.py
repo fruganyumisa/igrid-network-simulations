763de421cf006d1f1ab28aca617bd37bd3dbde7a
Bethuel <b@infolabs.dev>
2020-06-11 15:25:47 +0300

    Revert "change python string interpolation to py v2"

--- igrid-net.py
+++ igrid-net.py
@@ -5,16 +5,15 @@ from mininet.node import Controller
 from mininet.log import setLogLevel, info
 from mn_wifi.net import Mininet_wifi
 from mn_wifi.node import OVSKernelAP
-from mn_wifi.cli import CLI as CLI_wifi
-from mn_wifi.devices import DeviceTxPower as GetTxPower
+from mn_wifi.cli import CLI_wifi
+from mn_wifi.devices import GetTxPower
 from mn_wifi.link import wmediumd
 from mn_wifi.wmediumdConnector import interference
-from mn_wifi.propagationModels import PropagationModel
+from mn_wifi.propagationModels import propagationModel
 from utils import spawnStations
-from packets import *
 
 # command to start virtual interface to capture simulations traffic
-#
+# sh ifconfig hwsim0 up
 
 
 
@@ -34,7 +33,7 @@ def int2dpid( dpid ):
 def topology():
 
     print("Welcome to mininet-Wifi")
-    net = Mininet_wifi(controller = Controller, link=wmediumd, accessPoint =OVSKernelAP, noise_th=-91,  ac_method='sf')
+    net = Mininet_wifi(controller = Controller, link=wmediumd, accessPoint =OVSKernelAP, noise_threshold=-91, fading_coefficient=3)
 
 
    ## Hey mbaga here the for loop should be placed you can create a function in which when called with input maybe number of AP3 it uses a loop to create them
@@ -45,7 +44,20 @@ def topology():
     model='DI524',position = '40,30,0', range = '100')
     ap3 = net.addAccessPoint('ap3', ssid= 'iGrid-ap3', mode = 'g', channel = '3',
     model='DI524', position = '60,30,0', range = '100')
-
+   #  ap4 = net.addAccessPoint('ap4', ssid= 'iGrid-ap4', mode = 'g', channel = '4',
+   #  model='DI524', position = '80,30,0', range = '20')
+   #  ap5 = net.addAccessPoint('ap5', ssid= 'iGrid-ap5', mode = 'g', channel = '5',
+   #  model='DI524', position = '10,0,0', range = '20')
+   #  ap6 = net.addAccessPoint('ap6', ssid= 'iGrid-ap6', mode = 'g', channel = '6',
+   #  model='DI524', position = '40,60,0', range = '20')
+   #  ap7 = net.addAccessPoint('ap7', ssid= 'iGrid-ap7', mode = 'g', channel = '7',
+   #  model='DI524', position = '40,0,0', range = '20')
+   #  ap8 = net.addAccessPoint('ap8', ssid= 'iGrid-ap8', mode = 'g', channel = '8',
+   #  model='DI524', position = '80,30,0', range = '20')
+   #  ap9 = net.addAccessPoint('ap9', ssid= 'iGrid-ap9', mode = 'g', channel = '9',
+   #  model='DI524', position = '80,0,0', range = '20')
+   #  ap10 = net.addAccessPoint('ap10', ssid= 'iGrid-ap10', mode = 'g', channel = '10',
+   #  model='DI524', position = '80,60,0', range = '20')
 
     # Creating Stations (Smart meters, sensors and actuators)
 
@@ -59,6 +71,76 @@ def topology():
 
     net = spawnStations(net, stations=27, smart_meters=3, actuator=3, cidr="/8")
 
+   #  smeter1 = net.addStation('smeter1', mac = '00:00:00:00:00:01', ip = '10.0.0.1/8',
+   #  antennaHeight='1', antennaGain='5',position = '10,20,0',active_scan=1,scan_freq="2412 2437 2462")
+   #  smeter2 = net.addStation('smeter2', mac = '00:00:00:00:00:02', ip = '10.0.0.2/8',
+   #  antennaHeight='1', antennaGain='5',position = '28,30,0',active_scan=1,scan_freq="2412 2437 2462")
+   #  smeter3 = net.addStation('smeter3', mac = '00:00:00:00:00:03', ip = '10.0.0.3/8',
+   #  antennaHeight='1', antennaGain='5',  position = '10,40,0',active_scan=1,scan_freq="2412 2437 2462")
+   #  actuator1 = net.addStation('actuator1', mac = '00:00:00:00:00:04', ip = '10.0.0.4/8',
+   #  antennaHeight='1', antennaGain='5',  position = '50,40,0', active_scan=1,scan_freq="2412 2437 2462")
+   #  actuator2 = net.addStation('actuator2', mac = '00:00:00:00:00:05', ip = '10.0.0.5/8',
+   #  antennaHeight='1', antennaGain='5',  position = '50,40,0', active_scan=1,scan_freq="2412 2437 2462")
+   #  actuator3 = net.addStation('actuator3', mac = '00:00:00:00:00:06', ip = '10.0.0.6/8',
+   #  antennaHeight='1', antennaGain='5',  position = '50,40,0', active_scan=1,scan_freq="2412 2437 2462")
+   #  sta7 = net.addStation('sta7', mac = '00:00:00:00:00:07', ip = '10.0.0.7/8',
+   #  antennaHeight='1', antennaGain='5',  position = '50,40,0', active_scan=1,scan_freq="2412 2437 2462")
+   #  sta8 = net.addStation('sta8', mac = '00:00:00:00:00:08', ip = '10.0.0.8/8',
+   #  antennaHeight='1', antennaGain='5',  position = '50,40,0', active_scan=1,scan_freq="2412 2437 2462")
+   #  sta9 = net.addStation('sta9', mac = '00:00:00:00:00:09', ip = '10.0.0.9/8',
+   #  antennaHeight='1', antennaGain='5',  position = '50,40,0', active_scan=1,scan_freq="2412 2437 2462")
+   #  sta10 = net.addStation('sta10', mac = '00:00:00:00:00:10', ip = '10.0.0.10/8',
+   #  antennaHeight='1', antennaGain='5',  position = '50,40,0', active_scan=1,scan_freq="2412 2437 2462")
+   #  sta11 = net.addStation('sta11', mac = '00:00:00:00:00:11', ip = '10.0.0.11/8',
+   #  antennaHeight='1', antennaGain='5',  position = '50,40,0', active_scan=1,scan_freq="2412 2437 2462")
+   #  sta12 = net.addStation('sta12', mac = '00:00:00:00:00:12', ip = '10.0.0.12/8',
+   #  antennaHeight='1', antennaGain='5',  position = '50,40,0', active_scan=1,scan_freq="2412 2437 2462")
+   #  sta13 = net.addStation('sta13', mac = '00:00:00:00:00:13', ip = '10.0.0.13/8',
+   #  antennaHeight='1', antennaGain='5',  position = '50,40,0', active_scan=1,scan_freq="2412 2437 2462")
+   #  sta14 = net.addStation('sta14', mac = '00:00:00:00:00:14', ip = '10.0.0.14/8',
+   #  antennaHeight='1', antennaGain='5',  position = '50,40,0', active_scan=1,scan_freq="2412 2437 2462")
+   #  sta15 = net.addStation('sta15', mac = '00:00:00:00:00:15', ip = '10.0.0.15/8',
+   #  antennaHeight='1', antennaGain='5',  position = '50,40,0', active_scan=1,scan_freq="2412 2437 2462")
+   #  sta16 = net.addStation('sta16', mac = '00:00:00:00:00:16', ip = '10.0.0.16/8',
+   #  antennaHeight='1', antennaGain='5',  position = '50,40,0', active_scan=1,scan_freq="2412 2437 2462")
+   #  sta17 = net.addStation('sta17', mac = '00:00:00:00:00:17', ip = '10.0.0.17/8',
+   #  antennaHeight='1', antennaGain='5',  position = '50,40,0', active_scan=1,scan_freq="2412 2437 2462")
+   #  sta18 = net.addStation('sta18', mac = '00:00:00:00:00:18', ip = '10.0.0.18/8',
+   #  antennaHeight='1', antennaGain='5',  position = '50,40,0', active_scan=1,scan_freq="2412 2437 2462")
+   #  sta19 = net.addStation('sta19', mac = '00:00:00:00:00:19', ip = '10.0.0.19/8',
+   #  antennaHeight='1', antennaGain='5',  position = '50,40,0', active_scan=1,scan_freq="2412 2437 2462")
+   #  sta20 = net.addStation('sta20', mac = '00:00:00:00:00:20', ip = '10.0.0.20/8',
+   #  antennaHeight='1', antennaGain='5',  position = '50,40,0', active_scan=1,scan_freq="2412 2437 2462")
+   #  sta21 = net.addStation('sta21', mac = '00:00:00:00:00:21', ip = '10.0.0.21/8',
+   #  antennaHeight='1', antennaGain='5',  position = '50,40,0', active_scan=1,scan_freq="2412 2437 2462")
+   #  sta22 = net.addStation('sta22', mac = '00:00:00:00:00:22', ip = '10.0.0.22/8',
+   #  antennaHeight='1', antennaGain='5',  position = '50,40,0', active_scan=1,scan_freq="2412 2437 2462")
+   #  sta23 = net.addStation('sta23', mac = '00:00:00:00:00:23', ip = '10.0.0.23/8',
+   #  antennaHeight='1', antennaGain='5',  position = '50,40,0', active_scan=1,scan_freq="2412 2437 2462")
+   #  sta24 = net.addStation('sta24', mac = '00:00:00:00:00:24', ip = '10.0.0.24/8',
+   #  antennaHeight='1', antennaGain='5',  position = '50,40,0', active_scan=1,scan_freq="2412 2437 2462")
+   #  sta25 = net.addStation('sta25', mac = '00:00:00:00:00:25', ip = '10.0.0.25/8',
+   #  antennaHeight='1', antennaGain='5',  position = '50,40,0', active_scan=1,scan_freq="2412 2437 2462")
+   #  sta26 = net.addStation('sta26', mac = '00:00:00:00:00:26', ip = '10.0.0.26/8',
+   #  antennaHeight='1', antennaGain='5',  position = '50,40,0', active_scan=1,scan_freq="2412 2437 2462")
+   #  sta27 = net.addStation('sta27', mac = '00:00:00:00:00:27', ip = '10.0.0.27/8',
+   #  antennaHeight='1', antennaGain='5',  position = '50,40,0', active_scan=1,scan_freq="2412 2437 2462")
+   #  sta28 = net.addStation('sta28', mac = '00:00:00:00:00:28', ip = '10.0.0.28/8',
+   #  antennaHeight='1', antennaGain='5',  position = '50,40,0', active_scan=1,scan_freq="2412 2437 2462")
+   #  sta29 = net.addStation('sta29', mac = '00:00:00:00:00:29', ip = '10.0.0.29/8',
+   #  antennaHeight='1', antennaGain='5',  position = '50,40,0', active_scan=1,scan_freq="2412 2437 2462")
+   #  sta30 = net.addStation('sta30', mac = '00:00:00:00:00:30', ip = '10.0.0.29/8',
+   #  antennaHeight='1', antennaGain='5',  position = '50,40,0', active_scan=1,scan_freq="2412 2437 2462")
+   #  sta31 = net.addStation('sta31', mac = '00:00:00:00:00:31', ip = '10.0.0.31/8',
+   #  antennaHeight='1', antennaGain='5',  position = '50,40,0', active_scan=1,scan_freq="2412 2437 2462")
+   #  sta32 = net.addStation('sta32', mac = '00:00:00:00:00:32', ip = '10.0.0.32/8',
+   #  antennaHeight='1', antennaGain='5',  position = '50,40,0', active_scan=1,scan_freq="2412 2437 2462")
+   #  sta33 = net.addStation('sta33', mac = '00:00:00:00:00:33', ip = '10.0.0.33/8',
+   #  antennaHeight='1', antennaGain='5',  position = '50,40,0', active_scan=1,scan_freq="2412 2437 2462")
+   #  sta34 = net.addStation('sta34', mac = '00:00:00:00:00:34', ip = '10.0.0.34/8',
+   #  antennaHeight='1', antennaGain='5',  position = '50,40,0', active_scan=1,scan_freq="2412 2437 2462")
+
+
 
     cl = net.addController('cl', controller=Controller)
    
@@ -66,21 +148,19 @@ def topology():
     #Takes 5 minutes before to proceed
     #time.sleep(40)
     net.setPropagationModel(model="logDistance", exp=4)
-    #net.setModule('./mac80211_hwsim.ko')
 
 
 
     info("*** Configuring wifi nodes\n")
     net.configureWifiNodes()
-    #server=net.start_socket(ip='10.0.8.5', port=8080)
     
     #time.sleep(90)
 
-    net.plotGraph(max_x=300, max_y=300)
+    net.plotGraph(max_x=60, max_y=60)
 
     info("*** Enabling Association control (AP)\n")
     
-    
+    net.setAssociationCtrl(ac='ssf')
     net.auto_association()
 
 
@@ -88,10 +168,24 @@ def topology():
     net.addLink(ap1, sw)
     net.addLink(ap2, sw)
     net.addLink(ap3, sw)
-  
+   #  net.addLink(ap4, sw)
+   #  net.addLink(ap5, sw)
+   #  net.addLink(ap6, sw)
+   #  net.addLink(ap7, sw)
+   #  net.addLink(ap8, sw)
+   #  net.addLink(ap9, sw)
+   #  net.addLink(ap10, sw)
+    # net.addLink(ap1, sw)
+    # net.addLink(ap1, sw)
+    # net.addLink(ap1, sw)
+    # net.addLink(ap1, sw)
+    # net.addLink(ap1, sw)
 
     net.addLink(sw, server)
-  
+    #net.addLink(ap1, sta1)
+    #net.addLink(ap2, sta2)
+    #net.addLink(ap1,sta3)
+    #net.addLink(ap2,sta4)
 
     info("*** Starting network\n")
     net.build()
@@ -99,31 +193,33 @@ def topology():
     ap1.start([cl])
     ap2.start([cl])
     ap3.start([cl])
-
-    #net.cmd( "sh ifconfig hwsim0 up")
-    #os.system("sh ifconfig hwsim0 up")
-    #os.system("sudo wireshark")
-
-
-
-   #  info("Starting traffic\n")
-    
-   #  for i in range(33):
-   #     device_control =+2
-   #     ip_prefix ='10.0.0'
-   #     srcip  = '{}.{}'.format(ip_prefix, device_control)
-   #     dstip='10.0.8.5'
-   #     dstport=8080
-
-   #     send_packet(srip=srcip,dsip=dstip,dstport=dstport)
-    
-
-    
   
-  
+    #  ap4.start([cl])
+   #  ap5.start([cl])
+   #  ap6.start([cl])
+   #  ap7.start([cl])
+   #  ap8.start([cl])
+   #  ap9.start([cl])0.start([cl])
 
     info("*** Running CLI\n")
     CLI_wifi(net)
+
+    
+    x = 1
+
+    while x>= 1:
+       
+       if (x%3 == 0):
+          net.cmd('iperf smeter1 server')
+       else:
+         if(x%4 == 0):
+            smeter1.cmd('iperf actuator1 server')
+         else:
+            smeter1.cmd('ping 10.0.8.5')
+
+
+    
+
 
     info("*** Stopping the network \n")
     net.stop()
--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-def spawnStations(net, stations=0, smart_meters=0, actuator=0, mac_prefix="00:00:00:00:00", ip_prefix="10.0.0", cidr="/8"):
+def spawnStations(net, stations=0, smart_meters=0, actuator=0, mac_prefix="00:00:00:00:00", ip_prefix="10.0.0", cidr="/1"):
     """
     spawnStations generate specified number of IoT devices (stations) and assign to them (stations) 
     specific mac address and ip address based on mac_prefic and ip_prefix
@@ -29,25 +29,24 @@ def generate(net, device_control, device_total, prefix_name, mac_prefix, ip_pref
     """
     for i in range(device_total):
         device_control += 1
-        device_name = '{}{}'.format(prefix_name,i + 1)
-        mac_addr = '{}:{:0>2d}'.format(mac_prefix, device_control)
-        ip_addr = '{}.{}{}'.format(ip_prefix, device_control, cidr)
-        pos = '{},{},0'.format(device_control, device_control)
+        device_name = f'{prefix_name}{i + 1}'
+        mac_addr = f'{mac_prefix}:{"{:0>2d}".format(device_control)}'
+        ip_addr = f'{ip_prefix}.{device_control}{cidr}'
+        pos = f'{device_control},{device_control},0'
 
         print("Device: ", device_name, "mac_addr:",
               mac_addr, "ip_addr:", ip_addr, "position:", pos)
         net.addStation(device_name, mac=mac_addr, ip=ip_addr,
-                       antennaHeight='1', antennaGain='5',  position=pos)
-                       #, active_scan=1, scan_freq="2412 2437 2462"
+                       antennaHeight='1', antennaGain='5',  position=pos, active_scan=1, scan_freq="2412 2437 2462")
     return net
 
 
 def spawnAccessPoint(net, access_point=0):
     for i in range(access_point):
-        ap_name = 'ap{}'.format(i + 1)
-        ssid = 'iGrid-{}'.format(ap_name)
+        ap_name = f'ap{i + 1}'
+        ssid = f'iGrid-{ap_name}'
         curr = i + 1
-        pos = '{},{},0'.format(10 * curr, 30 * curr)
+        pos = f'{10 * curr},{30 * curr},0'
 
         print("ap_name:", ap_name, "ssid:", ssid, "pos:", pos)
         net.addAccessPoint(ap_name, ssid=ssid, mode='g', channel='6',
