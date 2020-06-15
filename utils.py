8f4810cb14183a74086895fda867495d5e674f26
bcmmbaga <b@infolabs.dev>
2020-06-15 21:30:04 +0300

    fix negative stations coordinate

--- utils.py
+++ utils.py
@@ -13,8 +13,8 @@ def spawnStations(net, sensors=0, smart_meters=0, actuator=0, mac_prefix="00:00:
     """
 
     # stations coordinates
-    coordinate = generateStationsCoordinates(
-        radius=100, count=sensors + smart_meters + actuator)
+    coordinate = generateStationsCoordinates(radius=100, reference=(
+        100, 100), count=sensors + smart_meters + actuator)
 
     #  generate smart meters
     net = generate(net, device_control=0, device_total=smart_meters, prefix_name="smeter",
@@ -44,8 +44,8 @@ def generate(net, device_control, device_total, prefix_name, mac_prefix, ip_pref
 
         print("Device: ", device_name, "mac_addr:",
               mac_addr, "ip_addr:", ip_addr, "position:", pos)
-        # net.addStation(device_name, mac=mac_addr, ip=ip_addr,
-        #                antennaHeight='1', antennaGain='5',  position=pos)
+        net.addStation(device_name, mac=mac_addr, ip=ip_addr,
+                       antennaHeight='1', antennaGain='5',  position=pos)
         # , active_scan=1, scan_freq="2412 2437 2462"
     return net
 
@@ -55,30 +55,22 @@ def spawnAccessPoint(net, access_point=0):
         ap_name = 'ap{}'.format(i + 1)
         ssid = 'iGrid-{}'.format(ap_name)
         curr = i + 1
-        pos = '{},{},0'.format(10 * curr, 30 * curr)
 
-        print("ap_name:", ap_name, "ssid:", ssid, "pos:", pos)
+        print("ap_name:", ap_name, "ssid:", ssid, "pos:", "100,100,0")
         net.addAccessPoint(ap_name, ssid=ssid, mode='g', channel='6',
-                           model='DI524', position=pos, range='100')
+                           model='DI524', position="100,100,0", range='100')
     return net
 
 
-def generateStationsCoordinates(radius, count):
+def generateStationsCoordinates(radius, reference=(0, 0), count=0):
     """
     Function generates a random station x, y coordinate which lies within the AP coverage radius
     and returns its X and Y coordinates as a tuple.
     """
     coordinates = []
     for i in range(0, count):
-        t = 2 * math.pi * random()
-        u = random() + random()
-        r = None
-
-        if u > 1:
-            r = 2 - u
-        else:
-            r = u
-
-        coordinates.append(
-            (radius * r * math.cos(t), radius * r * math.sin(t)))
+        r = radius * math.sqrt(random())
+        theta = 2 * math.pi * random()
+        coordinates.append(reference[0] + r * math.cos(theta),
+                           reference[1] * r * math.sin(theta))
     return coordinates
