39c0ec329c40aca7aff0d443d21b0cbf66342da1
bcmmbaga <b@infolabs.dev>
2020-07-17 12:24:44 +0300

    send packet at random inteval

--- packets.py
+++ packets.py
@@ -12,13 +12,12 @@ def create_packet(srip, dstip, dstport, payload='This is dummy traffic'):
 
 
 def send_packet(srip, destination={}, data="V = 240, I = 15, T = 08:40 PM V = 240, I = 15, T = 08:40 PM V = 240, I = 15, T = 08:40 PM V = 240, I = 15, T = 08:40 PM V = 240, I = 15, T = 08:40 PM V = 240, I = 15, T = 08:40 PM V = 240, I = 15, T = 08:40 PM V = 240, I = 15, T = 08:40 PM V = 240, I = 15, T = 08:40 PM V = 240, I = 15, T = 08:40 PM V = 240, I = 15, T = 08:40 PM V = 240, I = 15, T = 08:40 PM V = 240, I = 15, T = 08:40 PM V = 240, I = 15, T = 08:40 PM V = 240, I = 15, T = 08:40 PM V = 240, I = 15, T = 08:40 PM V = 240, I = 15, T = 08:40 PM V = 240, I = 15, T = 08:40 PM V = 240, I = 15, T = 08:40 PM"):
-    packet = create_packet(srip=srip, dstip=destination.get("ip"),
-                           dstport=destination.get("port"), payload=data)
-    send(packet, count=1, return_packets=True)
+    # packet = create_packet(srip=srip, dstip=destination.get("ip"),
+    #                        dstport=destination.get("port"), payload=data)
+    # send(packet, count=1, return_packets=True)
+    print(srip, destination)
 
 
 def unpack_send_packet(args):
     send_packet(*args)
 
-if __name__ == '__main__':
-    send_packet(srip='127.0.0.1', dsip='8.8.8.8', dstport=53)
--- traficgen.py
+++ traficgen.py
@@ -1,11 +1,13 @@
 #from packets import *
 import time
-from packets import unpack_send_packet
+from packets import unpack_send_packet, send_packet
 from multiprocessing import Pool
 import itertools
 import signal
 import sys
-
+import schedule
+import random
+import threading
 
 def generate_ip(start_ip=1, total_ips=25,):
     ip_array = []
@@ -13,11 +15,37 @@ def generate_ip(start_ip=1, total_ips=25,):
         ip_prefix = '10.0.0'
         start_ip = start_ip+1
         current_ip = '{}.{}'.format(ip_prefix, start_ip)
-        print(start_ip)
         ip_array.append(current_ip)
-        print(ip_array[ip])
 
     return ip_array
+
+
+def split_list(alist, wanted_parts=1):
+    random.shuffle(alist)
+    length = len(alist)
+    return [alist[i*length // wanted_parts: (i+1)*length // wanted_parts]
+            for i in range(wanted_parts)]
+
+
+def batch_schedule(batch=[], interval=1, destination={}):
+    for i in batch:
+        schedule.every(interval=interval).seconds.do(job_func=send_packet, srip=i, destination=destination).tag("sim")
+
+
+def node_send_in_interval():
+    destination = {
+        "ip": "10.0.0.55",
+        "port": 8080
+    }
+    ips_parts = split_list(alist=generate_ip(start_ip=1, total_ips=75), wanted_parts=10)
+
+    interval = 5
+    for part in ips_parts:
+        threading.Thread(target=batch_schedule, args=(part, interval,destination)).start()
+        interval=+5
+
+    while True:
+        schedule.run_pending()
 
 
 def node_sendpkts():
@@ -52,12 +80,15 @@ This fuction generates ips from a given number from specified starting last octe
         start_ip=76, total_ips=10), itertools.repeat(destination)))
     pool.close()
 
+
 def signal_handler(sig, frame):
     print 'You pressed Ctrl+C!'
+    schedule.clear(tag="sim")
     sys.exit(0)
 
 
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal_handler)
-    while True:
-        node_sendpkts()
+    # while True:
+    #     node_sendpkts()
+    node_send_in_interval()
