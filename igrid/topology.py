7c5c53793db4f16c1e64febd5c5e704a68b44480
bcmmbaga <b@infolabs.dev>
2020-07-27 21:22:02 +0300

    specifiy number of nodes at startup

--- igrid/topology.py
+++ igrid/topology.py
@@ -1,10 +1,11 @@
+import math
+from random import random
 from mininet.node import Controller
 from mn_wifi.net import Mininet_wifi
 from mn_wifi.link import wmediumd
 from mn_wifi.node import OVSKernelAP
 from mininet.log import setLogLevel, info
-from random import random
-import math
+from mn_wifi.cli import CLI as mininet_cli
 
 
 class IGRID:
@@ -15,48 +16,47 @@ class IGRID:
         self.sensors = []
         self.smart_meters = []
         self.actuators = []
+        self.access_points = []
 
-        # add access points
-        ap1 = self.net.addAccessPoint(
-            'ap1', ssid='iGrid-ap1', mode='g', channel='6', model='DI524', position='50,125,0', range='100')
-        ap2 = self.net.addAccessPoint(
-            'ap2', ssid='iGrid-ap2', mode='g', channel='1', model='DI524', position='100,75,0', range='100')
-        ap3 = self.net.addAccessPoint('ap3', ssid='iGrid-ap3', mode='g',
-                                      channel='3', model='DI524', position='125,125,0', range='100')
+        self.__addAccessPoints__()
 
         server = self.net.addHost(
             'server', mac='00:00:00:00:08:00', ip='10.10.10.254/8')
-        sw = self.net.addSwitch('sw',  dpid=self.__int2dpid(1))
 
-        cl = self.net.addController('cl', controller=Controller)
+        self.switch = self.net.addSwitch('sw',  dpid=self.__int2dpid__(1))
+
+        self.controller = self.net.addController('cl', controller=Controller)
 
         self.net.setPropagationModel(model="logDistance", exp=4)
 
-        # net.setModule('./mac80211_hwsim.ko')
         info("*** Configuring wifi nodes\n")
         self.net.configureWifiNodes()
         self.net.plotGraph(max_x=200, max_y=200)
 
         info("*** Enabling Association control (AP)\n")
-
         self.net.auto_association()
 
         info("*** Creating links and associations\n")
-        self.net.addLink(ap1, sw)
-        self.net.addLink(ap2, sw)
-        self.net.addLink(ap3, sw)
+        self.net.addLink(self.switch, server)
+        for ap in self.access_points:
+            self.net.addLink(ap, self.switch)
 
-        self.net.addLink(sw, server)
 
-        info("*** Starting network\n")
+
+    def start(self):
         self.net.build()
-        cl.start()
-        ap1.start([cl])
-        ap2.start([cl])
-        ap3.start([cl])
-        sw.start([cl])
+        self.controller.start()
+        self.switch.start([self.controller])
+        
+        for ap in self.access_points:
+            ap.start([self.controller])
 
-    def __int2dpid(self, dpid):
+        mininet_cli(self.net)
+
+    def stop(self):
+        self.net.stop()
+
+    def __int2dpid__(self, dpid):
         try:
             dpid = hex(dpid)[2:]
             dpid = '0' * (16 - len(dpid)) + dpid
@@ -66,7 +66,7 @@ class IGRID:
                             'please either specify a dpid or use a '
                             'canonical switch name such as s23.')
 
-    def __generate(self, net, device_control, device_total, prefix_name, mac_prefix, ip_prefix, cidr, coordinate):
+    def __generate__(self, net, device_control, device_total, prefix_name, mac_prefix, ip_prefix, cidr, coordinate):
         """
             generate creates station and add it to mininet.net object which contain all spawned network devices.
         """
@@ -79,12 +79,25 @@ class IGRID:
 
             print("Device: ", device_name, "mac_addr:",
                   mac_addr, "ip_addr:", ip_addr, "position:", pos)
-            net.addStation(device_name, mac=mac_addr, ip=ip_addr,
-                           antennaHeight='1', antennaGain='5',  position=pos)
+            self.net.addStation(device_name, mac=mac_addr, ip=ip_addr,
+                                antennaHeight='1', antennaGain='5',  position=pos)
             # , active_scan=1, scan_freq="2412 2437 2462"
         return net
 
-    def __generateStationsCoordinates(self, radius, reference=(0, 0)):
+    def __addAccessPoints__(self):
+        ap1 = self.net.addAccessPoint(
+            'ap1', ssid='iGrid-ap1', mode='g', channel='6', model='DI524', position='50,125,0', range='100')
+        self.access_points.append(ap1)
+
+        ap2 = self.net.addAccessPoint(
+            'ap2', ssid='iGrid-ap2', mode='g', channel='1', model='DI524', position='100,75,0', range='100')
+        self.access_points.append(ap2)
+
+        ap3 = self.net.addAccessPoint('ap3', ssid='iGrid-ap3', mode='g',
+                                      channel='3', model='DI524', position='125,125,0', range='100')
+        self.access_points.append(ap3)
+
+    def __generateStationsCoordinates__(self, radius, reference=(0, 0)):
         """
             generates a random station x, y coordinate which lies within the AP coverage radius
             and returns its X and Y coordinates as a tuple.
