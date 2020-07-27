6420f01d88789f2360cd472e0fed31c9481b4d78
bcmmbaga <b@infolabs.dev>
2020-07-27 23:32:03 +0300

    add network nodes

--- igrid/topology.py
+++ igrid/topology.py
@@ -1,5 +1,6 @@
 import math
 from random import random
+from netaddr import IPNetwork
 from mininet.node import Controller
 from mn_wifi.net import Mininet_wifi
 from mn_wifi.link import wmediumd
@@ -13,12 +14,18 @@ class IGRID:
                        accessPoint=OVSKernelAP, noise_th=-91,  ac_method='sf')
 
     def __init__(self, sensors=0, smart_meters=0, actuators=0):
-        self.sensors = []
-        self.smart_meters = []
-        self.actuators = []
-        self.access_points = []
+        self.sensors = sensors
+        self.smart_meters = smart_meters
+        self.actuators = actuators
+
+        self.sensors_nodes = []
+        self.smart_meters_nodes = []
+        self.actuators_nodes = []
+        self.access_points_nodes = []
 
         self.__addAccessPoints__()
+
+        self.__addNodes__()
 
         server = self.net.addHost(
             'server', mac='00:00:00:00:08:00', ip='10.10.10.254/8')
@@ -38,17 +45,15 @@ class IGRID:
 
         info("*** Creating links and associations\n")
         self.net.addLink(self.switch, server)
-        for ap in self.access_points:
+        for ap in self.access_points_nodes:
             self.net.addLink(ap, self.switch)
-
-
 
     def start(self):
         self.net.build()
         self.controller.start()
         self.switch.start([self.controller])
-        
-        for ap in self.access_points:
+
+        for ap in self.access_points_nodes:
             ap.start([self.controller])
 
         mininet_cli(self.net)
@@ -66,38 +71,83 @@ class IGRID:
                             'please either specify a dpid or use a '
                             'canonical switch name such as s23.')
 
-    def __generate__(self, net, device_control, device_total, prefix_name, mac_prefix, ip_prefix, cidr, coordinate):
+    def __addNodes__(self, network='10.0.0.0/8'):
         """
-            generate creates station and add it to mininet.net object which contain all spawned network devices.
+            creates nodes and add it to mininet.net object which contain all spawned network devices.
         """
-        for i in range(device_total):
-            device_control += 1
-            device_name = '{}{}'.format(prefix_name, i + 1)
-            mac_addr = '{}:{:0>2d}'.format(mac_prefix, device_control)
-            ip_addr = '{}.{}{}'.format(ip_prefix, device_control, cidr)
-            pos = '{},{},0'.format(coordinate[i][0], coordinate[i][1])
+        control = 0
+        total = self.sensors + self.smart_meters + self.actuators
 
-            print("Device: ", device_name, "mac_addr:",
-                  mac_addr, "ip_addr:", ip_addr, "position:", pos)
-            self.net.addStation(device_name, mac=mac_addr, ip=ip_addr,
-                                antennaHeight='1', antennaGain='5',  position=pos)
-            # , active_scan=1, scan_freq="2412 2437 2462"
-        return net
+        coordinates = self.__generateNodesCoordinates__(
+            radius=100, reference=(100, 100))
+
+        network = IPNetwork(network)
+
+        add_hosts = ((total//255) * 2) + 255 // (total % 255)
+        ip_addresses = [str(network[i]) for i in range(total + add_hosts) if not str(
+            network[i]).endswith('.0') and not str(network[i]).endswith('.255')]
+
+        # create sensor nodes
+        for i in range(self.sensors):
+            control += 1
+            device_name = 'sensor%d' % (i+1)
+            pos = '%s,%s,0' % (coordinates[i][0], coordinates[i][1])
+            ip_addr = ip_addresses[i]
+
+            print("Device: ", device_name, "ip_addr:",
+                  ip_addr, "position:", pos)
+
+            node = self.net.addStation(
+                device_name, ip=ip_addr, antennaHeight='1', antennaGain='5',  position=pos)
+
+            self.sensors_nodes.append(node)
+
+        # create smart meter nodes
+        for i in range(self.smart_meters):
+            device_name = 'meters%d' % (i+1)
+            pos = '%s,%s,0' % (
+                coordinates[control][0], coordinates[control][1])
+            ip_addr = ip_addresses[control]
+
+            print("Device: ", device_name, "ip_addr:",
+                  ip_addr, "position:", pos)
+
+            node = self.net.addStation(
+                device_name, ip=ip_addr, antennaHeight='1', antennaGain='5',  position=pos)
+
+            self.smart_meters_nodes.append(node)
+            control += 1
+
+        # create actuators nodes
+        for i in range(self.actuators):
+            device_name = 'actuator%d' % (i+1)
+            pos = '%s,%s,0' % (
+                coordinates[control][0], coordinates[control][1])
+            ip_addr = ip_addresses[control]
+
+            print("Device: ", device_name, "ip_addr:",
+                  ip_addr, "position:", pos)
+
+            node = self.net.addStation(
+                device_name, ip=ip_addr, antennaHeight='1', antennaGain='5',  position=pos)
+
+            self.actuators_nodes.append(node)
+            control += 1
 
     def __addAccessPoints__(self):
         ap1 = self.net.addAccessPoint(
             'ap1', ssid='iGrid-ap1', mode='g', channel='6', model='DI524', position='50,125,0', range='100')
-        self.access_points.append(ap1)
+        self.access_points_nodes.append(ap1)
 
         ap2 = self.net.addAccessPoint(
             'ap2', ssid='iGrid-ap2', mode='g', channel='1', model='DI524', position='100,75,0', range='100')
-        self.access_points.append(ap2)
+        self.access_points_nodes.append(ap2)
 
         ap3 = self.net.addAccessPoint('ap3', ssid='iGrid-ap3', mode='g',
                                       channel='3', model='DI524', position='125,125,0', range='100')
-        self.access_points.append(ap3)
+        self.access_points_nodes.append(ap3)
 
-    def __generateStationsCoordinates__(self, radius, reference=(0, 0)):
+    def __generateNodesCoordinates__(self, radius, reference=(0, 0)):
         """
             generates a random station x, y coordinate which lies within the AP coverage radius
             and returns its X and Y coordinates as a tuple.
