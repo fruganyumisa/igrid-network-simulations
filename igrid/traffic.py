23261f5f8d8f0fa5ef1dfa4c07fa22342b49b16c
bcmmbaga <b@infolabs.dev>
2020-08-15 16:04:54 +0300

    separate TCP and UDP packets

--- igrid/traffic.py
+++ igrid/traffic.py
@@ -13,22 +13,30 @@ class Traffic(IGRID):
         self.payload = "V = 240, I = 15, T = 08:40 PM V = 240, I = 15, T = 08:40 PM V = 240, I = 15, T = 08:40 PM V = 240, I = 15, T = 08:40 PM V = 240, I = 15, T = 08:40 PM V = 240, I = 15, T = 08:40 PM V = 240, I = 15, T = 08:40 PM V = 240, I = 15, T = 08:40 PM V = 240, I = 15, T = 08:40 PM V = 240, I = 15, T = 08:40 PM V = 240, I = 15, T = 08:40 PM V = 240, I = 15, T = 08:40 PM V = 240, I = 15, T = 08:40 PM V = 240, I = 15, T = 08:40 PM V = 240, I = 15, T = 08:40 PM V = 240, I = 15, T = 08:40 PM V = 240, I = 15, T = 08:40 PM V = 240, I = 15, T = 08:40 PM V = 240, I = 15, T = 08:40 PM"
         super(Traffic, self).__init__(sensors, smart_meters, actuators, max_x, max_y)
 
-    def __create_packet__(self, src, dst, port, payload):
+    def __create_TCP_packet__(self, src, dst, port, payload):
         return Ether()/IP(src=src, dst=dst)/TCP(dport=port)/payload
 
-    def __send_packet__(self, src, dst={}):
-        packet = self.__create_packet__(src=src, dst=dst.get(
+    def __create_UDP_packet__(self, src, dst, port, payload):
+        return Ether()/IP(src=src, dst=dst)/UDP(dport=port)/payload
+
+    def __send_TCP_packet__(self, src, dst={}):
+        packet = self.__create_TCP_packet__(src=src, dst=dst.get(
+            "ip"), port=dst.get("port"), payload=self.payload)
+        sendp(packet, count=1, return_packets=True, iface='sw-eth1')
+
+    def __send_UDP_packet__(self, src, dst={}):
+        packet = self.__create_UDP_packet__(src=src, dst=dst.get(
             "ip"), port=dst.get("port"), payload=self.payload)
         sendp(packet, count=1, return_packets=True, iface='sw-eth1')
 
 
-    def __unpack_send_packet__(self, args):
-        self.__send_packet__(*args)
+    def __unpack_send_TCP_packet__(self, args):
+        self.__send_TCP_packet__(*args)
 
     def __batch_schedule__(self, batch=[], interval=1, dst={}):
         for node in batch:
             schedule.every(interval=interval).seconds.do(
-                job_func=self.__send_packet__, src=str(node.IP()), dst=dst).tag("sim")
+                job_func=self.__send_UDP_packet__, src=str(node.IP()), dst=dst).tag("sim")
 
     def __split_nodes__(self, alist, wanted_parts=1):
         random.shuffle(alist)
@@ -43,7 +51,7 @@ class Traffic(IGRID):
         # sensors nodes send traffic
         sensors = [str(node.IP()) for node in self.sensors_nodes]
         pool = Pool(processes=len(sensors))
-        pool.map(self.__unpack_send_packet__, itertools.izip(
+        pool.map(self.__unpack_send_TCP_packet__, itertools.izip(
             sensors, itertools.repeat(dst)))
         pool.close()
 
@@ -52,7 +60,7 @@ class Traffic(IGRID):
         # smart meter nodes send trafic
         smart_meters = [str(node.IP()) for node in self.smart_meters_nodes]
         pool = Pool(processes=len(smart_meters))
-        pool.map(self.__unpack_send_packet__, itertools.izip(
+        pool.map(self.__unpack_send_TCP_packet__, itertools.izip(
             smart_meters, itertools.repeat(dst)))
         pool.close()
 
@@ -62,7 +70,7 @@ class Traffic(IGRID):
         actuators = [{"ip": str(node.IP()), "port": 8080}
                      for node in self.actuators_nodes]
         pool = Pool(processes=len(actuators))
-        pool.map(self.__unpack_send_packet__, itertools.izip(
+        pool.map(self.__unpack_send_TCP_packet__, itertools.izip(
             itertools.repeat(dst.get('ip')), actuators))
         pool.close()
 
